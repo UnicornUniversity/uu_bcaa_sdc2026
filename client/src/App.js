a50772bf20a8c55596533557f7adf036005ae95c
Ivo Milota <149657672+IMilota@users.noreply.github.com>
2026-05-16 19:15:00 +0200

    working example

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout";
-import Categories from "./categories/categories";
+import Categories from "./categories/categories.jsx";
+import Dashboard from "./transaction/dashboard";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<div>Transaction List</div>} />
+            <Route index element={<Dashboard />} />
             <Route path="/categoryList" element={<Categories />} />
           </Route>
         </Routes>
--- client/src/navigation.jsx
+++ client/src/navigation.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-import moneyIcon from "./money-pig-svgrepo-com.svg";
+import moneyIcon from "./money-with-wings-svgrepo-com.svg";
 
 function Navigation() {
   const navigate = useNavigate();
